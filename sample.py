9fcdb35be35ae3c824247e4f8b08a9038de90579
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2022-02-07 21:30:44 -0800

    Minor changes (untested)

--- sample.py
+++ sample.py
@@ -5,12 +5,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument(
-	'-p', '--percent', default=0.1, type=float, help='percentage (of 1.0) of input to output')
+	'-p', '--percent', default=10, type=float, help='percentage (of 100) of input to output')
 args = parser.parse_args()
 
 input_stream = io.TextIOWrapper(sys.stdin.buffer, newline=None, encoding='latin1')
 
 for line in input_stream:
 
-	if (random.random()  < args.percent):
+	if (random.random()  * 100 < args.percent):
 		print(line.strip())
